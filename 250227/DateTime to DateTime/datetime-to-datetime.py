4041159dc4f29d6d60e9818ffdb01d7086560c0f
한인혁 <138432939+R4mel@users.noreply.github.com>
2025-02-27 22:05:35 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,32 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+
+count = 0
+
+if a < 11:
+    print(-1)
+elif a == 11 and b < 11:
+    print(-1)
+elif a == 11 and b == 11 and c < 11:
+    print(-1)
+
+while True:
+    if a < 11:
+        break
+    elif a == 11 and b < 11:
+        break
+    elif a == 11 and b == 11 and c <= 11:
+        break
+
+    c -= 1
+    count += 1
+
+    if c == 0:
+        b -= 1
+        c = 60
+        if b == 0:
+            b = 24
+            a -= 1
+
+print(count)
