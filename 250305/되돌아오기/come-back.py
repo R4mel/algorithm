611421ac3200b979172580f4391ab6306014002f
한인혁 <138432939+R4mel@users.noreply.github.com>
2025-03-05 10:59:35 +0900

    Title: 되돌아오기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250305/되돌아오기/come-back.py
@@ -0,0 +1,38 @@
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+n = int(input())
+
+mapper = {
+    "E": 0,
+    "S": 1,
+    "W": 2,
+    "N": 3
+    # 시계방향
+}
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
+x = 0
+y = 0
+cnt = 0
+
+for i in range(n):
+    direction, dis = map(str, input().split())
+    dis = int(dis)
+    
+    move_dir = mapper[direction]
+
+    for _ in range(n):
+        x, y = x + dx[move_dir], y + dy[move_dir]
+
+        if x == 0 and y == 0:
+            break
+
+        if not in_range(x, y):
+            continue
+        else:
+            cnt += 1
+
+print(cnt)
